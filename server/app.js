8239fcf9ab92d56e010dd79913791008c19efaee
Kirtan Patel <mihirpatel1150@gmail.com>
2024-01-18 22:41:56 +0530

    Add necessary middlewares for server | add multer and mail functionalities

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
+import { errorHandler } from "./middlewares/error.middlewares.js";
+import rateLimit from "express-rate-limit";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const httpServer = createServer(app);
@@ -13,8 +16,38 @@ app.use(
   })
 );
 
+// Rate limiter to avoid misuse of the service and avoid cost spikes
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5000, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  keyGenerator: (req, res) => {
+    return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
+  },
+  handler: (_, __, ___, options) => {
+    throw new ApiError(
+      options.statusCode || 500,
+      `There are too many requests. You are only allowed ${
+        options.max
+      } requests per ${options.windowMs / 60000} minutes`
+    );
+  },
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public")); // configure static file to save images locally
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+// common error handling middleware
+app.use(errorHandler);
 
 export default httpServer;
