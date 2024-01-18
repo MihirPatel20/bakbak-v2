99c85a835d48c659c572a2392124e14931ede93e
Kirtan Patel <mihirpatel1150@gmail.com>
2024-01-18 21:30:16 +0530

    connect mongodb database

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { createServer } from "http";
 
 const app = express();
 const httpServer = createServer(app);
 
+// global middlewares
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is running!");
 });
 
 export default httpServer;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import httpServer from "./app.js";
+import connectDB from "./db/index.js";
 
 const startServer = () => {
-  httpServer.listen(8080, () => {
-    console.log(`⚙️  Server is  running on port 8080`);
+  httpServer.listen(process.env.PORT || 8080, () => {
+    console.log(`⚙️  Server is  running on port : ${process.env.PORT || 8080}`);
   });
 };
 
-startServer();
+try {
+  await connectDB();
+  startServer();
+} catch (error) {
+  console.log("Mongo db connect error: ", err);
+}
