9f56453bcd70785f4b8c25f616933a1be6da2527
Kirtan Patel <mihirpatel1150@gmail.com>
2024-03-01 12:20:37 +0530

    add routing in application using react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/register" element={<div>Register</div>} />
+        <Route path="/forgotpassword" element={<div>ForgotPassword</div>} />
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="*" render={() => <div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
