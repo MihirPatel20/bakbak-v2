b4507580da089214cdbb6e229ed28fc129cae489
mihir patel <mihirpatel1150@gmail.com>
2024-03-13 03:32:10 +0530

    server: cookies options change

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -147,7 +147,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const options = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    // sameSite: "none",
+    sameSite: "none",
   };
 
   res
