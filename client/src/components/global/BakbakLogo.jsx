342994c9bd715567e83fca1b403e86ccb29cd28c
mihir patel <mihirpatel1150@gmail.com>
2025-05-22 14:56:48 +0530

    feat(BakbakLogo): add navigation on logo click and improve cursor indication

--- client/src/components/global/BakbakLogo.jsx
+++ client/src/components/global/BakbakLogo.jsx
@@ -1,11 +1,14 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const BakbakLogo = ({ sx }) => {
+  const navigate = useNavigate();
+
   return (
     <Typography
       variant="body2"
-      // className="gradient-wave"
+      onClick={() => navigate("/")}
       sx={{
         fontWeight: "bold",
         fontFamily: "Outfit, sans-serif",
@@ -13,7 +16,7 @@ const BakbakLogo = ({ sx }) => {
         background: "linear-gradient(120deg, #007bff 75%, #00c6ff 100%)",
         WebkitBackgroundClip: "text",
         WebkitTextFillColor: "transparent",
-
+        cursor: "pointer", // Add cursor pointer to indicate clickable
         ...sx,
       }}
     >
