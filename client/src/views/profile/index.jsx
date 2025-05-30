35cc132eae1210a1868ee637c8de260dc3124fe7
mihir patel <mihirpatel1150@gmail.com>
2025-05-30 17:47:12 +0530

    feat(profile): add loading state with SkeletonProfileView and enhance layout for mobile and desktop

--- client/src/views/profile/index.jsx
+++ client/src/views/profile/index.jsx
@@ -7,6 +7,7 @@ import {
   Grid,
   Button,
   Container,
+  Skeleton,
   Stack,
   Link,
 } from "@mui/material";
@@ -22,12 +23,14 @@ import {
 import FolllowButton from "components/shared/FolllowButton";
 import PostGrid from "../post/PostGrid";
 import CoverImage from "./CoverImage";
+import { BrowserView, MobileView } from "react-device-detect";
+import SkeletonProfileView from "./SkeletonProfileView";
 
 const ProfilePage = () => {
   const [profile, setProfile] = useState(null);
+  const [showMore, setShowMore] = useState(false);
 
   const auth = useSelector((state) => state.auth);
-
   const navigate = useNavigate();
   const { username } = useParams();
   const currentProfileUsername = username || auth.user.username;
@@ -41,13 +44,8 @@ const ProfilePage = () => {
         console.error("Failed to fetch profile:", error);
       }
     };
-
     fetchProfile();
   }, [currentProfileUsername]);
-
-  if (!profile) {
-    return <Typography variant="h6">Loading...</Typography>;
-  }
 
   const getOrCreateChat = async (userId) => {
     try {
@@ -59,114 +57,211 @@ const ProfilePage = () => {
     }
   };
 
+  if (!profile) return <SkeletonProfileView />;
+
   const { followersCount, followingCount, totalPosts } = profile;
   const { bio, location, phoneNumber, account } = profile;
   const fullName = `${profile.firstName} ${profile.lastName}`;
 
   return (
-    <Container sx={{ px: 2 }}>
-      <CoverImage profile={profile} />
+    <Container>
+      <CoverImage
+        profile={profile}
+        sx={{
+          height: { xs: "100px", sm: "200px" },
+          borderRadius: 0.5,
+          // Add overlay container
+          "&::after": {
+            content: '""',
+            position: "absolute",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            background:
+              "linear-gradient(to top, rgba(0,0,0,0.3) 0%, rgba(0, 0, 0, 0.0) 60%)",
+            pointerEvents: "none", // Allows clicks to pass through
+            // zIndex: 1,
+          },
+        }}
+      />
 
-      <Grid container spacing={2}>
-        <Grid
-          item
-          //  sx={{ flex: { xs: 1, sm: 0 } }}
-        >
-          <Box
-            sx={{
-              display: "flex",
-              // flexDirection: { xs: "column", sm: "row" },
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              mt: -9,
-              ml: 3,
-            }}
-          >
-            <Avatar
-              src={getUserAvatarUrl(profile?.account?.avatar)}
-              sx={{ width: 150, height: 150, border: "5px solid white" }}
-            />
-            <OwnerComponent id={profile.account._id}>
-              <Button
-                variant="contained"
-                onClick={() => navigate(`/profile/edit/${profile.account._id}`)}
-              >
-                Edit Profile
-              </Button>
-            </OwnerComponent>
-          </Box>
-        </Grid>
-
-        <Grid item>
-          <Box
-            sx={{
-              ml: { xs: 0, sm: 5 },
-              mt: { xs: 2, sm: 2 },
-              display: "flex",
-              flexDirection: "column",
-            }}
-          >
-            <Typography variant="h1">{fullName}</Typography>
-            <Typography variant="body1" gutterBottom>
-              @{account.username}
-            </Typography>
-            <Typography variant="body1" gutterBottom>
-              {bio}
-            </Typography>
-            
-            {/* ToDo: allow user to add links and integrate with server */}
-            <Stack
-              direction="row"
-              spacing={1}
+      {/* MOBILE VIEW */}
+      <MobileView>
+        <Grid container direction="column" spacing={2}>
+          {/* Top Row: Avatar + Buttons */}
+          <Grid item>
+            <Box
               sx={{
-                mt: 1,
-                flexWrap: "wrap",
-                gap: 1,
+                display: "flex",
+                // justifyContent: "space-between",
+                alignItems: "center",
+                px: 0.5,
               }}
             >
-              {profile?.links?.map((link, index) => (
-                <Link
-                  key={index}
-                  href={link}
-                  target="_blank"
-                  rel="noopener noreferrer"
+              <Box sx={{ textAlign: "left", mt: -8 }}>
+                <Avatar
+                  src={getUserAvatarUrl(profile?.account?.avatar)}
                   sx={{
-                    color: "text.secondary",
-                    textDecoration: "none",
-                    "&:hover": {
-                      color: "primary.main",
-                      textDecoration: "underline",
-                    },
+                    width: 120,
+                    height: 120,
+                    border: "5px solid white",
                   }}
-                >
-                  {new URL(link).hostname.replace("www.", "")}
-                </Link>
-              ))}
-            </Stack>
-          </Box>
-        </Grid>
+                />
+              </Box>
 
-        <Grid item>
-          <VisitorComponent id={profile.account._id}>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 2, mt: 2 }}>
-              <FolllowButton profile={profile} />
-              <Button
-                disableElevation
-                size="small"
-                variant="contained"
-                onClick={() => {
-                  getOrCreateChat(profile.account._id);
-                }}
-              >
-                Message
-              </Button>
+              <Box sx={{ textAlign: "left", ml: 3, mt: -2 }}>
+                <OwnerComponent id={profile.account._id}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    onClick={() =>
+                      navigate(`/profile/edit/${profile.account._id}`)
+                    }
+                  >
+                    Edit Profile
+                  </Button>
+                </OwnerComponent>
+
+                <VisitorComponent id={profile.account._id}>
+                  <Box sx={{ display: "flex", gap: 1, mt: 1 }}>
+                    <FolllowButton profile={profile} />
+                    <Button
+                      // sx={{ borderRadius: 2 }}
+                      disableElevation
+                      size="small"
+                      variant="contained"
+                      onClick={() => getOrCreateChat(profile.account._id)}
+                    >
+                      Message
+                    </Button>
+                  </Box>
+                </VisitorComponent>
+              </Box>
             </Box>
-          </VisitorComponent>
-        </Grid>
-      </Grid>
 
-      <Divider sx={{ my: 2 }} />
+            {/* Buttons */}
+            <Box sx={{ textAlign: "left", ml: 2, mt: 1 }}>
+              <Grid item>
+                {/* Name, Username, Bio */}
+                <Typography variant="h4">{fullName}</Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  lineHeight={0.8}
+                  fontSize={12}
+                >
+                  @{account.username}
+                </Typography>
+                <Typography variant="body1" sx={{ mt: 1 }}>
+                  {bio}
+                </Typography>
+
+                {/* Links */}
+                <Stack
+                  direction="row"
+                  spacing={1}
+                  sx={{ flexWrap: "wrap", gap: 1, mt: 1 }}
+                >
+                  {profile?.links?.map((link, index) => (
+                    <Link
+                      key={index}
+                      href={link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {new URL(link).hostname.replace("www.", "")}
+                    </Link>
+                  ))}
+                </Stack>
+              </Grid>
+            </Box>
+          </Grid>
+        </Grid>
+      </MobileView>
+
+      {/* DESKTOP VIEW */}
+      <BrowserView>
+        {/* Main container */}
+        <Grid container spacing={2} px={3} sx={{ mt: -12, zIndex: 2 }}>
+          {/* Avatar + Edit button */}
+          <Grid item mt={-1}>
+            <Avatar
+              src={getUserAvatarUrl(profile?.account?.avatar)}
+              sx={{ width: 150, height: 150, border: "5px solid white", mb: 1 }}
+            />
+          </Grid>
+
+          {/* Right Side */}
+          <Grid item xs sx={{ zIndex: 2 }}>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="flex-start"
+              mt={2}
+              ml={3}
+            >
+              {/* Name + Username */}
+              <Box>
+                <Typography variant="h1">{fullName}</Typography>
+                <Typography variant="body1">@{account.username}</Typography>
+              </Box>
+
+              {/* Follow + Message / Edit -- Buttons */}
+              <Box>
+                <OwnerComponent id={profile.account._id}>
+                  <Button
+                    variant="contained"
+                    onClick={() =>
+                      navigate(`/profile/edit/${profile.account._id}`)
+                    }
+                  >
+                    Edit Profile
+                  </Button>
+                </OwnerComponent>
+                <VisitorComponent id={profile.account._id}>
+                  <Box display="flex" alignItems="center" gap={2}>
+                    <FolllowButton profile={profile} />
+                    <Button
+                      disableElevation
+                      size="small"
+                      variant="contained"
+                      onClick={() => getOrCreateChat(profile.account._id)}
+                    >
+                      Message
+                    </Button>
+                  </Box>
+                </VisitorComponent>
+              </Box>
+            </Box>
+
+            <Box mt={2} ml={3}>
+              {/* Bio */}
+              <Typography variant="body1">{bio}</Typography>
+
+              {/* Links */}
+              <Stack
+                direction="row"
+                spacing={1}
+                sx={{ flexWrap: "wrap", gap: 1, mt: 1 }}
+              >
+                {profile?.links?.map((link, index) => (
+                  <Link
+                    key={index}
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {new URL(link).hostname.replace("www.", "")}
+                  </Link>
+                ))}
+              </Stack>
+            </Box>
+          </Grid>
+        </Grid>
+      </BrowserView>
+
+      <Divider sx={{ mt: 2, mb: { xs: 1, sm: 2 } }} />
 
       <Grid container spacing={2}>
         <Grid
@@ -199,25 +294,26 @@ const ProfilePage = () => {
         </Grid>
       </Grid>
 
-      <Divider sx={{ my: 2 }} />
+      <Divider sx={{ mb: 2, mt: { xs: 1, sm: 2 } }} />
 
-      <Box
-        mb={4}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-        }}
-      >
+      <Box mb={4}>
         <Box
           display="flex"
           justifyContent="center"
           alignItems="center"
-          sx={{ m: 2 }}
+          my={{ xs: 1, sm: 2 }}
         >
-          <Typography variant="h1">Posts</Typography>
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: { xs: 20, sm: 32 },
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            Posts
+          </Typography>
         </Box>
-
         <PostGrid currentProfileUsername={currentProfileUsername} />
       </Box>
 
